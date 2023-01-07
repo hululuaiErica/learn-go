6af4160430839672293000ca45ea3442aa145578
Deng Ming <mingflycash@gmail.com>
2023-01-07 11:28:09 +0800

    分布式锁刷新和singleflight

--- cache/redis_lock.go
+++ cache/redis_lock.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	_ "embed"
 	"errors"
+	"fmt"
 	"github.com/go-redis/redis/v9"
 	"github.com/google/uuid"
+	"golang.org/x/sync/singleflight"
 	"time"
 )
 
@@ -15,16 +17,92 @@ var (
 
 	//go:embed lua/unlock.lua
 	luaUnlock string
+
+	//go:embed lua/refresh.lua
+	luaRefresh string
+
+	//go:embed lua/lock.lua
+	luaLock string
 )
 
 // Client 就是对 redis.Cmdable 的二次封装
 type Client struct {
 	client redis.Cmdable
+	//valGenerator func() string
+	g singleflight.Group
 }
 
 func NewClient(client redis.Cmdable) *Client {
 	return &Client{
 		client: client,
+	}
+}
+
+func (c *Client) SingleflightLock(ctx context.Context,
+	key string,
+	expiration time.Duration,
+	timeout time.Duration, retry RetryStrategy) (*Lock, error) {
+	for {
+		flag := false
+		resCh := c.g.DoChan(key, func() (interface{}, error) {
+			flag = true
+			return c.Lock(ctx, key, expiration, timeout, retry)
+		})
+		select {
+		case res := <- resCh:
+			if flag {
+				c.g.Forget(key)
+				if res.Err != nil {
+					return nil, res.Err
+				}
+				return res.Val.(*Lock), nil
+			}
+		case <- ctx.Done():
+			return nil, ctx.Err()
+		}
+	}
+}
+
+func (c *Client) Lock(ctx context.Context,
+	key string,
+	expiration time.Duration,
+	timeout time.Duration, retry RetryStrategy) (*Lock, error) {
+	var timer *time.Timer
+	val := uuid.New().String()
+	//val := c.valGenerator()
+	for {
+		// 在这里重试
+		lctx, cancel := context.WithTimeout(ctx, timeout)
+		res, err := c.client.Eval(lctx, luaLock, []string{key}, val, expiration.Seconds()).Result()
+		cancel()
+		if err != nil && !errors.Is(err, context.DeadlineExceeded) {
+			return nil, err
+		}
+
+		if res == "OK" {
+			return &Lock{
+				client: c.client,
+				key: key,
+				value: val,
+				expiration: expiration,
+				unlockChan: make(chan struct{}, 1),
+			}, nil
+		}
+
+		interval, ok := retry.Next()
+		if !ok {
+			return nil, fmt.Errorf("redis-lock: 超出重试限制, %w", ErrFailedToPreemptLock)
+		}
+		if timer == nil {
+			timer = time.NewTimer(interval)
+		} else {
+			timer.Reset(interval)
+		}
+		select {
+		case <- timer.C:
+		case <- ctx.Done():
+			return nil, ctx.Err()
+		}
 	}
 }
 
@@ -44,6 +122,8 @@ func (c *Client) TryLock(ctx context.Context,
 		client: c.client,
 		key: key,
 		value: val,
+		expiration: expiration,
+		unlockChan: make(chan struct{}, 1),
 	}, nil
 }
 
@@ -61,10 +141,70 @@ type Lock struct {
 	client redis.Cmdable
 	key string
 	value string
+	expiration time.Duration
+	unlockChan chan struct{}
+}
+
+func (l *Lock) AutoRefresh(interval time.Duration, timeout time.Duration) error {
+	timeoutChan := make(chan struct{}, 1)
+	// 间隔多久续约一次
+	ticker := time.NewTicker(interval)
+	for{
+		select {
+		case <- ticker.C:
+			// 刷新的超时时间怎么设置
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			// 出现了 error 了怎么办？
+			err := l.Refresh(ctx)
+			cancel()
+			if errors.Is(err, context.DeadlineExceeded) {
+				timeoutChan <- struct{}{}
+				continue
+			}
+			if err != nil {
+				return err
+			}
+		case <- timeoutChan:
+			// 刷新的超时时间怎么设置
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			// 出现了 error 了怎么办？
+			err := l.Refresh(ctx)
+			cancel()
+			if errors.Is(err, context.DeadlineExceeded) {
+				timeoutChan <- struct{}{}
+				continue
+			}
+			if err != nil {
+				return err
+			}
+
+		case <-l.unlockChan:
+			return nil
+		}
+	}
+}
+
+func (l *Lock) Refresh(ctx context.Context, ) error {
+	res, err := l.client.Eval(ctx, luaRefresh, []string{l.key}, l.value, l.expiration.Seconds()).Int64()
+	if err != nil {
+		return err
+	}
+	if res != 1 {
+		return ErrLockNotHold
+	}
+	return nil
 }
 
 func (l *Lock) Unlock(ctx context.Context) error {
 	res, err := l.client.Eval(ctx, luaUnlock, []string{l.key}, l.value).Int64()
+	defer func() {
+		//close(l.unlockChan)
+		select {
+		case l.unlockChan <- struct{}{}:
+		default:
+			// 说明没有人调用 AutoRefresh
+		}
+	}()
 	//if err == redis.Nil {
 	//	return ErrLockNotHold
 	//}
