9ce4c33ed23938c918c2bbc83342e415529a7e59
Deng Ming <mingflycash@gmail.com>
2023-04-16 20:26:08 +0800

    concurrent queue

--- live/queue/slice_queue.go
+++ live/queue/slice_queue.go
@@ -1,7 +1,9 @@
 package queue
 
 import (
+	"context"
 	"sync"
+	"time"
 )
 
 // SliceQueue 基于切片的实现
@@ -26,10 +28,21 @@ func NewSliceQueue[T any](capacity int) *SliceQueue[T] {
 	}
 }
 
-func (q *SliceQueue[T]) In(v T) error {
+func (q *SliceQueue[T]) Demo() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	q.In(ctx, q.zero)
+	cancel()
+}
+func (q *SliceQueue[T]) In(ctx context.Context, v T) error {
+
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
 
+	for q.isFull() {
+		// 如果是队列是满的，我就在这里等着
+		// 有人出队了，我就会被唤醒
+		q.notFull.Wait()
+	}
 	// 没有满才会往下执行
 	q.data[q.tail] = v
 	q.tail++
@@ -37,12 +50,22 @@ func (q *SliceQueue[T]) In(v T) error {
 	if q.tail == cap(q.data) {
 		q.tail = 0
 	}
+	// 我放了一个元素，我要通知另外一边准备出队的人
+	q.notEmpty.Signal()
 	return nil
 }
 
-func (q *SliceQueue[T]) Out() (T, error) {
+func (q *SliceQueue[T]) Out(ctx context.Context) (T, error) {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
+
+	for q.isEmpty() {
+		// 如果队列是 empty，我就在这儿等着
+		// 有人入队了，我就会被唤醒
+		q.notEmpty.Wait()
+
+		// 我被唤醒的时候，队列里面是不是一定有元素
+	}
 
 	// 不空才会往下执行
 	front := q.data[q.head]
@@ -53,6 +76,8 @@ func (q *SliceQueue[T]) Out() (T, error) {
 	if q.head == cap(q.data) {
 		q.head = 0
 	}
+	// 我拿走了一个元素，我就唤醒对面在等待空位的人
+	q.notFull.Signal()
 	return front, nil
 }
 
