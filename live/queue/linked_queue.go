b3759e2d64b1617706070b86ff5c1f37ca70044a
Deng Ming <mingflycash@gmail.com>
2023-05-07 20:26:04 +0800

    5.7 linkedqueue - updated

--- live/queue/linked_queue.go
+++ live/queue/linked_queue.go
@@ -1,19 +1,16 @@
 package queue
 
-import "context"
-
 // 基于链表的，非阻塞的，使用锁的并发队列
 
 type LinkedQueue[T any] struct {
 }
 
-func (l *LinkedQueue[T]) In(ctx context.Context, val T) error {
+func (l *LinkedQueue[T]) In(val T) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (l *LinkedQueue[T]) Out(ctx context.Context) (T, error) {
+func (l *LinkedQueue[T]) Out() (T, error) {
 	//TODO implement me
 	panic("implement me")
 }
-
