5415ed10b64087673d4590741d4550e9c8b20aba
Deng Ming <ming_flycash@qq.com>
2023-08-07 16:26:34 +0800

    fix bug

--- cache/retry.go
+++ cache/retry.go
@@ -10,15 +10,14 @@ type RetryStrategy interface {
 
 type FixedIntervalRetryStrategy struct {
 	Interval time.Duration
-	MaxCnt int
-	cnt int
+	MaxCnt   int
+	cnt      int
 }
 
 func (f *FixedIntervalRetryStrategy) Next() (time.Duration, bool) {
 	if f.cnt >= f.MaxCnt {
 		return 0, false
 	}
+	f.cnt++
 	return f.Interval, true
 }
-
-
