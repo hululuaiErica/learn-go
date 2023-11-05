5f0af39886254c9eca33f1663626ac01dd4090be
Deng Ming <ming_flycash@qq.com>
2023-11-05 21:56:29 +0800

    11.5

--- live/queue/concurrent_linked_queue.go
+++ live/queue/concurrent_linked_queue.go
@@ -33,10 +33,13 @@ func (c *ConcurrentLinkedQueue[T]) Enqueue(ctx context.Context, val T) error {
 	for {
 		tailPtr := atomic.LoadPointer(&c.tail)
 		tail := (*node[T])(tailPtr)
-		tailNextPtr := atomic.LoadPointer(&tail.next)
+		//tailNextPtr := atomic.LoadPointer(&tail.next)
+		//if tailNextPtr != nil {
+		//	continue
+		//}
 
 		// 这是一个原子 CAS 操作
-		if atomic.CompareAndSwapPointer(&tail.next, tailNextPtr, newNodePtr) {
+		if atomic.CompareAndSwapPointer(&tail.next, nil, newNodePtr) {
 			// 说明你入队成功了
 
 			// 可能失败，失败意味着，后面的 goroutine 已经修正了 c.tail
@@ -47,20 +50,28 @@ func (c *ConcurrentLinkedQueue[T]) Enqueue(ctx context.Context, val T) error {
 	//tail.next = newNode
 	// 这边又是一个原子 CAS 操作
 	//c.tail = newNode
-
 }
 
 func (c *ConcurrentLinkedQueue[T]) Dequeue(ctx context.Context) (T, error) {
 	// 读取 head 和 tail，你要原子操作
 	// 读完之后比较
-	if c.head == c.tail {
-		return c.zero, ErrEmptyQueue
+	// 同一时刻的有 N 个 goroutine，最终 for 循环的次数应该是
+	// n + (n-1) + (n-2) + (n-3) + ... + 1
+	// n(n+1)/2
+	for {
+		// LongAdder
+		headPtr := atomic.LoadPointer(&c.head)
+		if headPtr == atomic.LoadPointer(&c.tail) {
+			return c.zero, ErrEmptyQueue
+		}
+		head := (*node[T])(headPtr)
+		headNextPtr := atomic.LoadPointer(&head.next)
+		// 这边又是一个 CAS 操作
+		if atomic.CompareAndSwapPointer(&c.head, headPtr, headNextPtr) {
+			headNext := (*node[T])(headNextPtr)
+			return headNext.data, nil
+		}
 	}
-	head := c.head
-	// 这边又是一个 CAS 操作
-	headNext := head.next
-	c.head.next = headNext.next
-	return headNext.data, nil
 }
 
 type node[T any] struct {
